67bb77c0aee298717fe86260a3f3ce56b0c5c592
Santiago Balcero <santiagobalcero@gmail.com>
2025-08-10 11:26:09 -0500

    ping redis in consumer

--- consumer-api/service/service.go
+++ consumer-api/service/service.go
@@ -2,12 +2,18 @@ package service
 
 import (
 	"context"
+	"log"
 	"outbox-consumer/redis"
 
 	redisclient "github.com/redis/go-redis/v9"
 )
 
 func FetchOrderEvents() {
+	if err := redis.Client.Ping(context.Background()).Err(); err != nil {
+		log.Println("Error pinging Redis:", err)
+		return
+	}
+
 	subscription := redis.Client.Subscribe(context.Background(), "pizza-orders")
 	messagesch := subscription.Channel()
 
